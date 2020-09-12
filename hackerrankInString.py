456dd546a071029dd957f66874a7d001185fcdbb
Chris <chrishood374@msn.com>
2020-09-12 23:06:00 +0100

    updated

--- hackerrankInString.py
+++ hackerrankInString.py
@@ -2,6 +2,7 @@
 #We say that a string contains the word hackerrank if a subsequence of its characters spell the word hackerrank. 
 #For example, if string s="hhaacckkeerrannkk" it does contain hackerrank, but s="haackerank" does not. 
 # In the second case, the second r is missing. If we reorder the first string as , it no longer contains the subsequence due to ordering.
+#For each query, print YES on a new line if the string contains hackerrank, otherwise, print NO. 
 
 #s = input string
 
